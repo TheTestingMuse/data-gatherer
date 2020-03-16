36d7fcc7bd2b724e54f63ce97024b21b319023d1
David Williams <david@thetestingmuse.uk>
2020-03-16 18:12:20 +0000

    Various fixes

--- sportinglife/spiders/cards_spider.py
+++ sportinglife/spiders/cards_spider.py
@@ -1,17 +1,17 @@
 import os
-from typing import re
+import re
 
 import scrapy
-from yarl import URL
-
-# Define base domain
-url = URL('https://www.sportinglife.com')
 
 # Remove existing racecards.json file
 if os.path.exists("racecards.json"):
     os.remove("racecards.json")
 else:
     print("The file does not exist")
+
+
+def is_handicap(race_name):
+    return "Handicap" in race_name or "Nursery" in race_name
 
 
 # Define RaceDetails Item
@@ -31,70 +31,63 @@ class RaceDetails(scrapy.Item):
 
 class RacecardsSpider(scrapy.Spider):
     name = "racecards"
-    allowed_domains = ['www.sportinglife.com']
+    allowed_domains = ["sportinglife.com"]
     start_urls = [
-        'https://www.sportinglife.com/racing/racecards',
+        "https://www.sportinglife.com/racing/racecards",
     ]
 
     def parse(self, response):
         # Define Valid Meeting names
-        valid_meetings = ["Aintree", "Ascot", "Ayr", "Ballinrobe", "Bangor-On-Dee", "Bath", "Bellewstown",
-                          "Beverley", "Brighton", "Carlisle", "Cartmel", "Catterick", "Chelmsford City", "Cheltenham",
-                          "Chepstow",
-                          "Chester", "Clonmel", "Cork", "Curragh", "Doncaster", "Down Royal", "Downpatrick",
-                          "Dundalk", "Epsom Downs", "Exeter", "Fairyhouse", "Fakenham", "Ffos Las", "Folkestone",
-                          "Fontwell", "Galway", "Goodwood", "Gowran", "Great Leighs", "Hamilton", "Haydock",
-                          "Hereford", "Hexham", "Huntingdon", "Kelso", "Kempton", "Kilbeggan", "Killarney",
-                          "Laytown", "Leicester", "Leopardstown", "Limerick", "Lingfield", "Listowel", "Ludlow",
-                          "Market Rasen", "Musselburgh", "Naas", "Navan", "Newbury", "Newcastle", "Newmarket",
-                          "Newton Abbot", "Nottingham", "Perth", "Plumpton", "Pontefract", "Punchestown", "Redcar",
-                          "Ripon", "Roscommon", "Salisbury", "Sandown", "Sedgefield", "Sligo", "Southwell",
-                          "Stratford", "Taunton", "Thirsk", "Thurles", "Tipperary", "Towcester", "Tralee", "Tramore",
-                          "Uttoxeter", "Warwick", "Wetherby", "Wexford", "Wincanton", "Windsor", "Wolverhampton",
-                          "Worcester", "Yarmouth", "York"]
-
+        valid_meetings = [
+            "Aintree", "Ascot", "Ayr", "Ballinrobe", "Bangor-On-Dee", "Bath", "Bellewstown",
+            "Beverley", "Brighton", "Carlisle", "Cartmel", "Catterick", "Chelmsford City",
+            "Cheltenham", "Chepstow", "Chester", "Clonmel", "Cork", "Curragh", "Doncaster",
+            "Down Royal", "Downpatrick", "Dundalk", "Epsom Downs", "Exeter", "Fairyhouse",
+            "Fakenham", "Ffos Las", "Folkestone", "Fontwell", "Galway", "Goodwood", "Gowran",
+            "Great Leighs", "Hamilton", "Haydock", "Hereford", "Hexham", "Huntingdon", "Kelso",
+            "Kempton", "Kilbeggan", "Killarney", "Laytown", "Leicester", "Leopardstown", "Limerick",
+            "Lingfield", "Listowel", "Ludlow", "Market Rasen", "Musselburgh", "Naas", "Navan",
+            "Newbury", "Newcastle", "Newmarket", "Newton Abbot", "Nottingham", "Perth", "Plumpton",
+            "Pontefract", "Punchestown", "Redcar", "Ripon", "Roscommon", "Salisbury", "Sandown",
+            "Sedgefield", "Sligo", "Southwell", "Stratford", "Taunton", "Thirsk", "Thurles",
+            "Tipperary", "Towcester", "Tralee", "Tramore", "Uttoxeter", "Warwick", "Wetherby",
+            "Wexford", "Wincanton", "Windsor", "Wolverhampton", "Worcester", "Yarmouth", "York",
+        ]
         # Begin looping through cards for each Meeting on page
-        for card in response.css('section.hr-meeting-container'):
+        for card in response.css("section.hr-meeting-container"):
+            # Get Meeting-level data - Title, Going and Surface
+            current_meeting = card.css("h2.sectionTitleWithProviderLogo a::text").get()
+            going = card.css("div.hr-meeting-meta span.hr-meeting-meta-value::text").get()
+            surface = card.css("div.hr-meeting-meta-surface span.hr-meeting-meta-value::text").get()
 
-            # Check Meeting Title to see if it's a UK/IRE race meet
-            current_meeting = card.css('h2.sectionTitleWithProviderLogo a::text').get()
+            # Check if Meeting is UK/IRE and loop through individual races
+            if not any(s in current_meeting for s in valid_meetings):
+                continue
 
-            # Begin looping through individual races
-            if any(s in current_meeting for s in valid_meetings):
-
+            for race_item in card.css("ul.hr-meeting-races-container li"):
                 # Get Meeting details
                 r = RaceDetails()
-                r["raceMeeting"] = card.css('h2.sectionTitleWithProviderLogo a::text').get()
-                r["raceGoing"] = card.css('div.hr-meeting-meta span.hr-meeting-meta-value::text').get()
-                r["raceSurface"] = card.css('div.hr-meeting-meta-surface span.hr-meeting-meta-value::text').get()
-                r["raceTime"] = card.css('span.hr-meeting-race-time::text').get()
+                r["raceMeeting"] = current_meeting
+                r["raceGoing"] = going
+                r["raceSurface"] = surface
 
-                temp_race_name = card.css('div.hr-meeting-race-name-star::text').get()
-                r["raceName"] = temp_race_name
-                if "Handicap" in temp_race_name or "Nursery" in temp_race_name:
-                    r["raceHandicap"] = "Handicap"
-                else:
-                    r["raceHandicap"] = "Non-Handicap"
+                # Get Races details
+                r["raceTime"] = race_item.css("span.hr-meeting-race-time::text").get()
+                r["raceName"] = race_item.css("div.hr-meeting-race-name-star::text").get()
+                r["raceHandicap"] = (
+                    "Handicap" if is_handicap(r["raceName"]) else "Non-Handicap"
+                )
+                race_link = race_item.css("a::attr(href)").get()
+                r["raceLink"] = ("https://www.sportinglife.com" + race_link)
 
-                r["raceLink"] = 'https://www.sportinglife.com' + card.css(
-                    'ul.hr-meeting-races-container a::attr(href)').extract_first()
-                r["runnersAge"] = card.css('div.hr-meeting-race-name-star span::text')[1].extract().split(",")[0]
-
+                # Split Ages, Classes, Race Length and Runners
+                #race_details = race_item.css("div.hr-meeting-race-name-star span::text")[1].extract()
+                ages, *extra, length, runners = re.split(r',\s', race_item.css("div.hr-meeting-race-name-star span::text")[1].extract())
                 # TO DO: Remove "Yo" text and account for Range of years
-
+                r["runnersAge"] = ages
                 # Check if Class data is missing. Mark as Class 0 if so and bump element location
-                temp_race_class = card.css('div.hr-meeting-race-name-star span::text')[1].extract().split(", ")[1]
-                if "Class" in temp_race_class:
-                    r["raceClass"] = temp_race_class[-1]
-                    r["raceLength"] = card.css('div.hr-meeting-race-name-star span::text')[1].extract().split(", ")[2]
-                    temp_race_runners = card.css('div.hr-meeting-race-name-star span::text')[1].extract().split(", ")[
-                        3].replace(' runners', '')
-                    r["raceRunners"] = temp_race_runners
-                else:
-                    r["raceClass"] = "0"
-                    r["raceLength"] = card.css('div.hr-meeting-race-name-star span::text')[1].extract().split(", ")[1]
-                    temp_race_runners = card.css('div.hr-meeting-race-name-star span::text')[1].extract().split(", ")[
-                        2].replace(' runners', '')
-                    r["raceRunners"] = temp_race_runners
+                r["raceClass"] = extra[0][-1] if extra else "0"
+                r["raceLength"] = length
+                r["raceRunners"] = runners.replace(" runners", "")
 
                 yield r
